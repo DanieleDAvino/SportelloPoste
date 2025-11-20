86d475846010fe35da31b391936cffbd42375468
frida <frida.valecchi@risikamente.it>
2025-11-20 20:00:19 +0100

    primo commit 1 solo sportello

---
+++ src/ListaClienti.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+/**
+ * Classe che rappresenta la risorsa condivisa fra i due thread
+ * da gestire con metodi "synchronized"
+ * e con l'uso di wait() e notify()
+ * @author frida
+ * @version 1.0
+ */
+public class ListaClienti {
+    private ArrayList<Integer> listaNumeri;
+    private int ultimoArrivo;
+    private int ultimoServito;
+    private final int numeroMassimo = 2;
+    /**
+     * constructor
+     * settaggio delle variabili di istanza
+     */
+    public ListaClienti() {
+        listaNumeri = new ArrayList<Integer>();
+        ultimoArrivo = 0;
+        ultimoServito = 0;
+    }
+
+    /*synchronized parola chiave che gestisce il meccanismo del lock ovvero
+    * 1) impedisce ad un altro thread l'esecuzione di tale
+    * metodo, se un precedente thread lo sta già eseguendo
+    * 2) senza di lui wait e notify non possono essere usati
+    * si genera l'eccezione : IllegalMonitorStateException,*/
+
+    /**
+     * TODO: cosa fa?
+     *
+     */
+    public synchronized Integer rimuoviCliente() throws
+            InterruptedException {
+        while (ultimoServito >= ultimoArrivo) {
+            System.out.println("non ci sono arrivi dopo l'ultimo servito");
+            wait();
+        }
+        ultimoServito++;
+        return ultimoServito;
+    }
+
+    /**
+     * TODO: cosa fa?
+     *
+     */
+    public synchronized Integer addCliente() {
+        if (ultimoArrivo < numeroMassimo) {
+            ultimoArrivo++;
+            listaNumeri.add(ultimoArrivo);
+            notify();
+            return ultimoArrivo;
+        }
+        return null;
+    }
+}
